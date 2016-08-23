08704b82f00d2c624b430a44e1cf923cce9e8471
LLTommy <Thomas.Liener@felix-solution.com>
2016-08-23 13:59:05 +0100

    Changes to some files, this is very early release state

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 
 gulp.task('uglify', function(){
- console.log("This is the task uglify from gulp, this MIGHT TAKE A WHILE");
+ console.log("This is the task uglify from gulp");
  return gulp.src('build/ols-treeview.js')
  .pipe(uglify())
  //.pipe(rename({extname: '.min.js'}))
